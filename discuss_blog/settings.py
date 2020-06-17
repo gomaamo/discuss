dac02367a4c7baba486d7c7e21e9c87db3fc376a
mohamed gomaa <mamadomedhat@gmail.com>
2020-06-17 03:07:17 +0200

    m

--- discuss_blog/settings.py
+++ discuss_blog/settings.py
@@ -10,10 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import django_heroku
 import os
-
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
